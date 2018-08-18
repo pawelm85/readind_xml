71629598c471c75292645b9099d46ddf34c36b35
Paweł <pawelm85@yahoo.pl>
2018-08-18 14:05:55 +0200

    clean_file

--- source/Program.cs
+++ source/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml.Linq;
 
 namespace WyszukiwanieXML
@@ -11,6 +8,7 @@ namespace WyszukiwanieXML
     {
         static void Main(string[] args)
         {
+            #region xml - pojedyncze mieszkanie
             string inputXmlSingle =
            @"
 <mieszkania>
@@ -33,7 +31,10 @@ namespace WyszukiwanieXML
    </mieszkanie>
 </mieszkania>
   ";
+            #endregion
 
+
+            #region xml - więcej mieszkań
             string inputXmlMulti =
                 @"<mieszkania>
 <mieszkanie id='1'>
@@ -77,29 +78,32 @@ namespace WyszukiwanieXML
 
 </mieszkania>
 ";
+            #endregion
+
+
 
             XElement element = XElement.Parse(inputXmlMulti);
 
-            //***********przykład 1*****************
+            //  Przykład 1 - pobieranie tylko wartości typ dla każdego pokoju
             //var pokoje = from p in element.Descendants("pokoj") select  p.Element("typ").Value;
             //foreach (var pokoj in pokoje)
             //{
             //    Console.WriteLine(pokoj);
             //}
 
-            //***********przykład 2*****************
+            // Przykład 2 - pobieranie dla każdego pokoju wartości typ i wartości pow
             //var pokoje = from pokoj in element.Descendants("pokoj") select pokoj;
-            var pokoje = element.Descendants("pokoj").Select(p => p);           
+            //var pokoje = element.Descendants("pokoj").Select(p => p);           
 
-            foreach (var pokoj in pokoje)
-            {
+            //foreach (var pokoj in pokoje)
+            //{
                 //Console.WriteLine(pokoj);
                 //Console.WriteLine();
                 //Console.WriteLine($"{pokoj.Descendants("mieszkanie").Elements("pokoj")}");
                // Console.WriteLine($"=> {pokoj.Element("typ").Value} powierzchnia: {pokoj.Element("pow").Value}");
-            }
+            //}
 
-            //***********przykład 3*****************
+            // Przykład 3 - pobieranie id mieszkania i typ oraz pow poszczególnych pokoi
             var mieszkania = element.Descendants("mieszkanie").Select(m=>m);
             foreach(var mieszkanie in mieszkania)
             {
